272c87749fb2d1a518e7a55adb9c53c1ae4c1a72
giuliano-st <71044058+giuliano-st@users.noreply.github.com>
2026-08-12 17:25:45 -0300

    Resolução Exercicio1
    Atualização 12/08/26

--- Exercicios1/Exercicio1.py
+++ Exercicios1/Exercicio1.py
@@ -15,23 +15,33 @@ lista2 = []
 lista3 = []
 lista4 = []
 
-listaR = []
-for i in range(10000):
-    listaR.append(random.randint(1, 1000))
+listaR = [random.randint(1, 1000) for _ in range(10000)]
 
-def separa_lista(lista, ini, fim):
-    for i in range(ini, fim):
-        lista.append(listaR[i])
+lista_somas = []
 
-t1 = threading.Thread(target=separa_lista, args=(lista1, 0, 2499))
-t2 = threading.Thread(target=separa_lista, args=(lista2, 2500, 4999))
-t3 = threading.Thread(target=separa_lista, args=(lista3, 5000, 7499))
-t4 = threading.Thread(target=separa_lista, args=(lista3, 7500, 9999))
+def separa_lista(lista_final, ini, fim, lista_inicial):
+    for i in range(ini, fim + 1):
+        lista_final.append(lista_inicial[i])
+
+        soma = sum(lista_final)
+        lista_somas.append(soma)
+
+t1 = threading.Thread(target=separa_lista, args=(lista1, 0, 2499, listaR))
+t2 = threading.Thread(target=separa_lista, args=(lista2, 2500, 4999, listaR))
+t3 = threading.Thread(target=separa_lista, args=(lista3, 5000, 7499, listaR))
+t4 = threading.Thread(target=separa_lista, args=(lista4, 7500, 9999, listaR))
 
 t1.start()
 t2.start()
 t3.start()
 t4.start()
+
+t1.join()
+t2.join()
+t3.join()
+t4.join()
+
+soma_final = sum(lista_somas)
 
 print(lista1)
 print("------")
@@ -42,3 +52,5 @@ print("------")
 print(lista4)
 print("------")
 print(listaR)
+
+print(f"Soma das listas: {soma_final}")
