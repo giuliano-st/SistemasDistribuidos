b65954dbd64ba77783e8f5acf3ffc05157159430
giuliano-st <71044058+giuliano-st@users.noreply.github.com>
2026-08-19 20:20:57 -0300

    Adição de Random
    Atualização 19/08

--- Atividade1/Modulo2/controller/FranquiaController.java
+++ Atividade1/Modulo2/controller/FranquiaController.java
@@ -2,8 +2,10 @@ package Atividade1.Modulo2.controller;
 
 import Atividade1.Modulo2.service.FranquiaService;
 import Atividade1.Modulo2.view.FranquiaView;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class FranquiaController {
+    int randomico = ThreadLocalRandom.current().nextInt(9000, 9999 + 1);
     private FranquiaView view;
     private FranquiaService service;
 
@@ -15,7 +17,7 @@ public class FranquiaController {
     public void calcularFaturamentos() {
 
         try {
-            double faturamentoTotal = service.calcularFaturamentoTotal(4, 10000);
+            double faturamentoTotal = service.calcularFaturamentoTotal(4, randomico);
             view.exibirFaturamentoFinal(faturamentoTotal);
 
         } catch (InterruptedException e) {
